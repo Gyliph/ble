86a594335210b749ad26451bc40458d3a0f5a160
Jeffrey King <jmuking@gmail.com>
2017-12-02 14:38:18 -0600

    Jeff's commit
    Changes to postprocess.m 
    +Adding findpeak parameters

--- postprocess.m
+++ postprocess.m
@@ -9,27 +9,36 @@ data = csvread('csv2.out');
 
 % butterworth filter 6th order 
 % 3-dB cutoff freq 0.7Hz to 10Hz - 100hz sample rate
-[b a] = butter(6, 0.3, 'low');
+sf = 100; sf2 = sf/2;
+[b a] = butter(6, 15/sf2);
 data_l = filter(b,a,data);
 
 subplot(3,1,1);
 plot(data_l);
 
-[b a] = butter(6, [0.7 10]/(100/2));
+[b a] = butter(6, [0.7/sf2 5/sf2]);
 data_filtered = filter(b,a,data_l);
 x = data_filtered(:,1);
 y = data_filtered(:,2);
+z = data_filtered(:,3);
 window_width = 30;
 window_increment = 1;
 num_steps = (length(data_filtered)-window_width+1)-1;
 for i = 1:window_increment:num_steps;
 	xe(i,1) = sumsq(x(i:i+window_width));
 	ye(i,1) = sumsq(y(i:i+window_width));
+  %if(xe(i,1) > 0.05) %Get rid of spikes JEFF KING
+  %  xe(i,1) = 0.05;
+  %end
+  %if(ye(i,1) > 0.05) %Get rid of spikes JEFF KING
+  %  ye(i,1) = 0.05;
+  %end
 end
 
-% find peaks
-[pks_x idx_x] = findpeaks(xe);
-[pks_y idx_y] = findpeaks(ye);
+% find peak
+% added MinPeakDistance JEFF KING
+[pks_x idx_x] = findpeaks(xe, "MinPeakDistance", 42, "MinPeakHeight", 0.005); 
+[pks_y idx_y] = findpeaks(ye, "MinPeakDistance", 42, "MinPeakHeight", 0.005);
 
 % plot peaks on top of signal
 subplot(3,1,2);
@@ -46,8 +55,9 @@ for i = 1:p_window_increment:p_num_steps-1;
   xt = xe(i:i+p_window_width);
   yt = ye(i:i+p_window_width);
   % Find peaks in window
-  [pks_x idx_x] = findpeaks(xt);
-  [pks_y idx_y] = findpeaks(yt);
+  % added MinPeakDistance JEFF KING
+  [pks_x idx_x] = findpeaks(xt, "MinPeakDistance", 42, "MinPeakHeight", 0.005);
+  [pks_y idx_y] = findpeaks(yt, "MinPeakDistance", 42, "MinPeakHeight", 0.005);
   % Estimate pulse rate from peaks
   % find distance between peaks
   if(length(idx_x) > 1) 
@@ -59,7 +69,7 @@ for i = 1:p_window_increment:p_num_steps-1;
    end
    subplot(3,1,2);
    plot(xt, 'b', idx_x, pks_x, '.r');
-   subplot(3,1,3); 
+   subplot(3,1,3);
    plot(yt, 'g', idx_y, pks_y, '.r');
    waitforbuttonpress;
    
